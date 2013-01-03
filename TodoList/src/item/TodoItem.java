36422590aadce68d98197558a1499ce810b3e05a
Liratanak <annliratanak@gmail.com>
2013-01-03 12:33:49 +0700

    Todo item

--- TodoList/src/item/TodoItem.java
+++ TodoList/src/item/TodoItem.java
@@ -1,12 +1,70 @@
 package item;
 
+import java.util.Date;
+
 public class TodoItem {
 
-	/**
-	 * @param args
-	 */
+	private int id;
+	private int version;
+	private int priority;
+	private Date lastUpdate;
+	private Date deadline;
+	private String object;
+
+	public TodoItem() {
+		this.priority = 1;
+		this.lastUpdate = new Date();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	public Date getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+
+	public String getObject() {
+		return object;
+	}
+
+	public void setObject(String object) {
+		this.object = object;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
