2636527eabc27a60a1cb1568c3c328a59c75ffa7
Liratanak <annliratanak@gmail.com>
2013-02-02 21:17:24 +0700

    add constructor

--- TodoListClient/src/main/MainTodoListClient.java
+++ TodoListClient/src/main/MainTodoListClient.java
@@ -4,8 +4,6 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-
 import action.Client;
 
 import pkinterface.IClient;
--- TodoListServer/src/server/TodoListServer.java
+++ TodoListServer/src/server/TodoListServer.java
@@ -2,55 +2,69 @@ package server;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.List;
 
 import pkinterface.IClient;
 import pkinterface.TodoItemInterface;
 import pkinterface.TodoListServerInterface;
 
-
-public class TodoListServer implements TodoListServerInterface{
+public class TodoListServer implements TodoListServerInterface {
 	List<TodoItemInterface> todoList;
 	List<IClient> clientList;
-	
+
+	public TodoListServer() {
+		this.todoList = new ArrayList<>();
+		this.clientList = new ArrayList<>();
+	}
+
 	public List<TodoItemInterface> getTodoList() {
 		return todoList;
 	}
+
 	public void setTodoList(List<TodoItemInterface> todoList) {
 		this.todoList = todoList;
 	}
+
 	public List<IClient> getClientList() {
 		return clientList;
 	}
+
 	public void setClientList(List<IClient> clientList) {
 		this.clientList = clientList;
 	}
-	
+
 	@Override
 	public TodoItemInterface getTodo(int id) throws RemoteException {
 		return this.todoList.get(id);
 	}
+
 	@Override
 	public List<TodoItemInterface> getList(IClient source) throws RemoteException {
-		for (IClient client : clientList) {
-			
+		if (clientList.contains(source)) {
+			return this.todoList;
 		}
+		this.clientList.add(source);
 		return this.todoList;
 	}
+
 	@Override
 	public void disconnect(IClient source) throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		if(this.clientList.contains(source)){
+			this.clientList.remove(source);
+		}
 	}
+
 	@Override
 	public int updateTodo(TodoItemInterface item, IClient source) throws RemoteException {
-		// TODO Auto-generated method stub
+
 		return 0;
 	}
+
 	@Override
-	public boolean deleteTodo(TodoItemInterface ti, IClient source) throws RemoteException {
+	public boolean deleteTodo(TodoItemInterface item, IClient source) throws RemoteException {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
 }
